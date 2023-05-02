bd103cd5afe9c980731238f8f0e826e5ce04530e
Wendy GERVAIS <kau.grv@gmail.com>
2023-05-03 01:19:16 +0200

    Sprites Assets psd + début import dans Unity

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,5 @@
+using System.Net.Mime;
+using System.Threading;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +9,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Application.targetFrameRate = 16;
     }
 
     // Update is called once per frame
