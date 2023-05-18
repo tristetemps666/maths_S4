34d75c56bf3ac80935c30f6cd4a8aeb14973e449
TRISTAN <debeaunetristan@gmail.com>
2023-05-18 12:14:07 +0200

    bernouilli stats OK test Bernoulli good

--- Assets/Scripts/Random/Bernouilli.cs
+++ Assets/Scripts/Random/Bernouilli.cs
@@ -5,19 +5,53 @@ using MyRand;
 
 
 
-namespace BERNOUILLI
+namespace BERNOULLI
 {
+    public struct BernoulliStats{
+
+        public BernoulliStats(int val=0){
+            roll_number = val;
+            number_of_win = 0;
+            number_of_loose = 0;
+            average_win = 0;  
+        }
+
+        public int roll_number;
+        
+        public int number_of_win;
+
+        public int number_of_loose;
+
+        public float average_win;
+    }
     
-    public class Bernouilli {
+    public class Bernoulli {
 
         private float win_proba = 0.5f;
         private int win_number = 1;
-        private int loose_number = 0; 
+        private int loose_number = 0;
+
+        private BernoulliStats  stats = new BernoulliStats(0);
+
+
+        public Bernoulli() {
+            stats = new BernoulliStats(0); // sombre un peu obligé de mettre un param dans le constructeur wsh
+        }
 
 
 
         public int rand(){
-            return myRand.rand_0_1() <=win_proba ? win_number : loose_number;
+            int val = myRand.rand_0_1() <=win_proba ? win_number : loose_number;
+            update_stats(val);
+            return val;
+        }
+
+
+        private void update_stats(int result){
+            if(result==1) stats.number_of_win++;
+            else stats.number_of_loose++;
+            stats.average_win = (stats.average_win*stats.roll_number+result)/(stats.roll_number+1f);
+            stats.roll_number++;
         }
 
         public float esperance(){
@@ -26,6 +60,10 @@ namespace BERNOUILLI
 
         public float variance(){
             return win_proba*(1-win_proba);
+        }
+
+        public BernoulliStats get_stats(){
+            return stats;
         }
     }
 }
--- Assets/Scripts/Random/test_proba.cs
+++ Assets/Scripts/Random/test_proba.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using DISCRETE_UNIFORM;
+using BERNOULLI;
 using MyRand;
 
 public class test_proba : MonoBehaviour
 {
     // Start is called before the first frame update
     public DiscreteUniform dice;
+    public Bernoulli piece;
 
 
     private string to_string(List<int> list){
@@ -25,21 +27,34 @@ public class test_proba : MonoBehaviour
     void Start()
     {
         if (dice == null) dice = new DiscreteUniform(); 
+        if (piece == null) piece = new Bernoulli();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        // DICE TEST
         DiscreteUniformStats stats = dice.get_stats();
+        BernoulliStats stats_ber = piece.get_stats();
+
+
         if(Input.GetKeyDown(KeyCode.Space)){
             Debug.Log("dé : " + dice.rand()+ "\n\n");
             // Debug.Log("rand 0 1 : " + myRand.rand_0_1());
 
         }
-
         if (Input.GetKeyDown(KeyCode.A)){
             Debug.Log("stats : \n nombre de lancers : "+stats.roll_number+"\n nombre pour chq faces : "+ to_string(stats.amount_foreach_number) + "\n face(s) la plus fréquente : " + to_string(stats.most_frequent_number) + "\n nombre moyen : "+ stats.average_number);
+        }
+
+
+        // COIN TEST
+        if(Input.GetKeyDown(KeyCode.KeypadEnter)){
+            Debug.Log("piece : " + piece.rand()+ "\n\n");
+        }
+        if(Input.GetKeyDown(KeyCode.Z)){
+            Debug.Log("stats : \n nombre de lancers : "+stats_ber.roll_number+"\n nombre de win : "+ stats_ber.number_of_win + "\n nombre de looses : "  + stats_ber.number_of_loose + "\n nombre moyen : "+ stats_ber.average_win);
         }
     }
 }
