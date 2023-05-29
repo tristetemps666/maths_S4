b36df1aa04e324c33e2177ba68780e67f732ba7d
TRISTAN <debeaunetristan@gmail.com>
2023-05-29 20:41:20 +0200

    good for presentation

--- Assets/Scripts/end_game.cs
+++ Assets/Scripts/end_game.cs
@@ -25,27 +25,36 @@ public class end_game : MonoBehaviour
     public Sprite full_heart_sprite;
 
     public TextMeshPro metal_text;
+    public TextMeshPro stats_text;
 
     public string[] names_metal = new string[4] {"","bronze","argent","or"};
+    public string[] names_games = new string[4]{"de","fakir","piece","bar"};
     public int player_money = 0;
 
     public TextMeshPro player_money_text;
 
     public Transform start_bar_money;
 
-    public int[] list_count_games_played = new int[4]{0,0,0,0};
+    public List<int> list_count_games_played = new List<int>{0,0,0,0};
 
+    public List<int> list_gain = new List<int> {0,0,0,0};
+
+    public List<float> list_gain_moyen = new List<float> {0,0,0,0};
+
+    public int max_money;
     public int max_money_earned;
 
-    public int int_max_money_lost;
+    public int max_money_lost;
+
+    public int min_money = 200;
 
     private float ratio_money = 0f;
 
     
-    public BernoulliStats bs = new BernoulliStats(0);
-    public ExponentielleStats es = new ExponentielleStats(0);
+    public BernoulliStats bs = new BernoulliStats();
+    public ExponentielleStats es = new ExponentielleStats();
 
-    public DiscreteUniformStats us = new DiscreteUniformStats(0);
+    public DiscreteUniformStats us = new DiscreteUniformStats();
     void Start()
     {
         stats_page.SetActive(false);
@@ -62,6 +71,7 @@ public class end_game : MonoBehaviour
 
         if(stat_button.is_activated){
             stats_page.SetActive(true);
+            set_text_stats();
         }
 
         if(score_button.is_activated){
@@ -99,9 +109,23 @@ public class end_game : MonoBehaviour
     }
 
     void set_text_stats(){
-        void set_text_stats_dice(){
-            string text = "dice :\n nombre de lancers : "+us.roll_number+"\n nombre pour chq faces : "+ to_string(us.amount_foreach_number) + "\n face(s) la plus fréquente : " + to_string(us.most_frequent_number) + "\n nombre moyen : "+ us.average_number;
-        }
+        Debug.Log("set les stats");
+        // set_text_stats_dice();
+        string text = "argent max : " + max_money.ToString() + "\n";
+        text+= "argent min : " + min_money.ToString() + "\n\n";
+        text+= "meilleur gain : " + max_money_earned.ToString() +"\n";
+        text+= "pire perte : " + max_money_lost.ToString()+ "\n\n";
+        text+= "jeu le plus gagnant : " +names_games[get_max(list_gain).Item1]+ "\n";
+        text+= "jeu le moins gagnant : " +names_games[get_min(list_gain).Item1]+ "\n";
+
+        stats_text.text = text;
+    }
+
+
+    void set_text_stats_dice(){
+        string text = "dice :\n nombre de lancers : "+us.roll_number+"\n nombre pour chq faces : "+ to_string(us.amount_foreach_number) + "\n face(s) la plus fréquente : " + to_string(us.most_frequent_number) + "\n nombre moyen : "+ us.average_number;
+
+        stats_text.text = text;
     }
 
 
@@ -118,5 +142,21 @@ public class end_game : MonoBehaviour
             }
             return a + list[list.Count-1].ToString() +"}";
         }
+    }
+
+    private (int,int) get_max(List<int> list){ // (index,value)
+        (int,int) res = (0,list[0]);
+        for(int i=0; i<list.Count; i++){
+            if(list[i] > res.Item2) res = (i,list[i]);
+        }
+        return res;
+    }
+
+    private (int,int) get_min(List<int> list){ // (index,value)
+        (int,int) res = (0,list[0]);
+        for(int i=0; i<list.Count; i++){
+            if(list[i] < res.Item2) res = (i,list[i]);
+        }
+        return res;
     }
 }
